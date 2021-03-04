ba177ee0349587582e2daf4f978ab04a3b3a1554
pa4080 <spas.z.spasov@gmail.com>
2021-03-04 11:59:00 +0200

    copy_js_update

--- modules/TypographyCleanClipboard.js
+++ modules/TypographyCleanClipboard.js
@@ -35,50 +35,68 @@
 jQuery( function( $ ) {
 	'use strict';
 
+    // https://stackoverflow.com/questions/7215479/get-parent-element-of-a-selected-text
+    function getSelectionParentElement() {
+        var parentEl = null, sel;
+        if (window.getSelection) {
+            sel = window.getSelection();
+            if (sel.rangeCount) {
+                parentEl = sel.getRangeAt(0).commonAncestorContainer;
+                if (parentEl.nodeType != 1) {
+                    parentEl = parentEl.parentNode;
+                }
+            }
+        } else if ( (sel = document.selection) && sel.type != "Control") {
+            parentEl = sel.createRange().parentElement();
+        }
+        return parentEl;
+    }
+
+    // From wp-Typography by Peter Putzer
 	if ( window.getSelection ) {
 
 		document.addEventListener( 'copy', function() {
 
-		    //alert(document.activeElement);
+            if (getSelectionParentElement().tagName !== 'PRE') {
 
-		    if (![ 'INPUT', 'TEXTAREA', 'PRE' ].includes(document.activeElement.tagName)) {
+		        if (![ 'INPUT', 'TEXTAREA', 'PRE' ].includes(document.activeElement.tagName)) {
 
-			var
-				sel        = window.getSelection(),
-				ranges     = [],
-				rangeCount = sel.rangeCount,
-				i, shadow;
+    		    	var
+	    		    	sel        = window.getSelection(),
+		    		    ranges     = [],
+    			    	rangeCount = sel.rangeCount,
+	    			    i, shadow;
 
-			for ( i = 0; i < rangeCount; i++ ) {
-				 ranges[i] = sel.getRangeAt( i );
-			}
+    	    		for ( i = 0; i < rangeCount; i++ ) {
+	    	    		ranges[i] = sel.getRangeAt( i );
+		    	    }
 
-			// Create new div containing cleaned HTML content
-			shadow = $( '<div>', {
-				style: { position: 'absolute', left: '-99999px' },
-				html: $( '<div></div>' ).append( sel.getRangeAt( 0 ).cloneContents() ).html().replace( /\u00AD/gi, '' ).replace( /\u200B/gi, '' )
-			} );
+    			    // Create new div containing cleaned HTML content
+        			shadow = $( '<div>', {
+	        			style: { position: 'absolute', left: '-99999px' },
+		        		html: $( '<div></div>' ).append( sel.getRangeAt( 0 ).cloneContents() ).html().replace( /\u00AD/gi, '' ).replace( /\u200B/gi, '' )
+			        } );
 
-			// Append to DOM
-			$( 'body' ).append( shadow );
+        			// Append to DOM
+	        		$( 'body' ).append( shadow );
 
-			// Select the children of our "clean" div
-			sel.selectAllChildren( shadow[0] );
+		        	// Select the children of our "clean" div
+			        sel.selectAllChildren( shadow[0] );
 
-			// Clean up after copy
-			window.setTimeout( function() {
+        			// Clean up after copy
+	        		window.setTimeout( function() {
 
-				// Remove div
-				shadow.remove();
+		        		// Remove div
+			        	shadow.remove();
 
-				// Restore selection
-				sel.removeAllRanges();
-				for ( i = 0; i < rangeCount; i++ ) {
-					 sel.addRange( ranges[i] );
-				}
-			}, 0 );
-
-		    }
+				        // Restore selection
+				        sel.removeAllRanges();
+        				for ( i = 0; i < rangeCount; i++ ) {
+	        				 sel.addRange( ranges[i] );
+		        		}
+			        }, 0 );
+    		    }
+            }
 		} );
 	}
 } );
