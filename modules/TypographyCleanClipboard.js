19db1bfb5d17a23d20e0fdb188cd7964f23f5e2c
pa4080 <spas.z.spasov@gmail.com>
2021-03-04 14:51:55 +0200

    copy/paste script update

--- modules/TypographyCleanClipboard.js
+++ modules/TypographyCleanClipboard.js
@@ -59,43 +59,42 @@ jQuery( function( $ ) {
 
             if (getSelectionParentElement().tagName !== 'PRE') {
 
-		        if (![ 'INPUT', 'TEXTAREA', 'PRE' ].includes(document.activeElement.tagName)) {
+		    	var
+    		    	sel        = window.getSelection(),
+	    		    ranges     = [],
+			    	rangeCount = sel.rangeCount,
+    			    i, shadow;
 
-    		    	var
-	    		    	sel        = window.getSelection(),
-		    		    ranges     = [],
-    			    	rangeCount = sel.rangeCount,
-	    			    i, shadow;
+	    		for ( i = 0; i < rangeCount; i++ ) {
+    	    		ranges[i] = sel.getRangeAt( i );
+	    	    }
 
-    	    		for ( i = 0; i < rangeCount; i++ ) {
-	    	    		ranges[i] = sel.getRangeAt( i );
-		    	    }
+			    // Create new div containing cleaned HTML content
+    			shadow = $( '<div>', {
+        			style: { position: 'absolute', left: '-99999px' },
+	        		html: $( '<div></div>' ).append( sel.getRangeAt( 0 ).cloneContents() ).html().replace( /\u00AD/gi, '' ).replace( /\u200B/gi, '' )   // default action
+                            .replace( /(<span class=\"mw-editsection\">(<([^>]+)>).*<\/span>)/gi, '' )                                                  // skinTimeless
+                            .replace( /(<div id=\"(mw-page-header-links|mw-site-navigation|mw-related-navigation)\">(<([^>]+)>).*<\/div>)/gi, '' )      // skinTimeless
+		        } );
 
-    			    // Create new div containing cleaned HTML content
-        			shadow = $( '<div>', {
-	        			style: { position: 'absolute', left: '-99999px' },
-		        		html: $( '<div></div>' ).append( sel.getRangeAt( 0 ).cloneContents() ).html().replace( /\u00AD/gi, '' ).replace( /\u200B/gi, '' )
-			        } );
+    			// Append to DOM
+        		$( 'body' ).append( shadow );
 
-        			// Append to DOM
-	        		$( 'body' ).append( shadow );
+	        	// Select the children of our "clean" div
+		        sel.selectAllChildren( shadow[0] );
 
-		        	// Select the children of our "clean" div
-			        sel.selectAllChildren( shadow[0] );
+    			// Clean up after copy
+        		window.setTimeout( function() {
 
-        			// Clean up after copy
-	        		window.setTimeout( function() {
+	        		// Remove div
+		        	shadow.remove();
 
-		        		// Remove div
-			        	shadow.remove();
-
-				        // Restore selection
-				        sel.removeAllRanges();
-        				for ( i = 0; i < rangeCount; i++ ) {
-	        				 sel.addRange( ranges[i] );
-		        		}
-			        }, 0 );
-    		    }
+			        // Restore selection
+			        sel.removeAllRanges();
+    				for ( i = 0; i < rangeCount; i++ ) {
+        				 sel.addRange( ranges[i] );
+	        		}
+		        }, 0 );
             }
 		} );
 	}
